2ca1b2e493dd8c06ae82b34fac2ea10576d0fbf6
Raghul9521 <yeswanthraghul@gmail.com>
2022-03-10 09:00:54 +0530

    Updates

---
+++ precache-manifest.03cfc96a6d8b10148929140d98000e6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef77cfc80e4e73ec26c05966967ff4b",
+    "url": "/reactkanban/index.html"
+  },
+  {
+    "revision": "c11d45cfa9b557be188d",
+    "url": "/reactkanban/static/js/2.ff47b5f2.chunk.js"
+  },
+  {
+    "revision": "ecd196325ad2c2b2d1fb",
+    "url": "/reactkanban/static/js/main.7897bab4.chunk.js"
+  },
+  {
+    "revision": "8fefa6c32dac426612ab",
+    "url": "/reactkanban/static/js/runtime~main.a4ed7714.js"
+  }
+]);
